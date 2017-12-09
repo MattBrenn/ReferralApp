804740c827d9b710bcc329c0a52a2b1a2e834640
MattBrenn <matthewbrenn@hotmail.com>
2017-12-09 21:03:38 +0000

    global

---
+++ ReferralApp/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using ReferralApp.Models;
+
+namespace ReferralApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Logger.Info("Referral App Started");
+
+        }
+
+        protected void Application_Error()
+        {
+            var exception = Server.GetLastError();
+
+            Logger.Error(exception);
+        }
+    }
+}
